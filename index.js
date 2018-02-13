54c5a0e71efdc082c71e01ccade475817674797b
Morgan V Hirsch <morganhirsch93@hotmail.co.uk>
2018-02-13 16:19:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ function takeANumber(katzDeliLine, newCustomer){
 }
 
 function nowServing(katzDeliLine){
+  if(katzDeliLine.length > 1){
   katzDeliLine.shift();
+  return `Currently serving ${katzDeliLine[0]} `
+  } else{
   return "There is nobody waiting to be served!"
+  }
 }
