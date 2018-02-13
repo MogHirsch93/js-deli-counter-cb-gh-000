014eda8226182728069d57a48e11c37554853c0d
Morgan V Hirsch <morganhirsch93@hotmail.co.uk>
2018-02-13 16:43:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function nowServing(katzDeliLine){
 
 function currentLine(katzDeliLine, customer){
   let currentLineString = 'The line is currently: '
-  if(katzDeliLine.length ====0){
+  if(katzDeliLine.length === 0){
     return "The line is currently empty.";
   }
   for(let i = 0; i < katzDeliLine.length; i++){
