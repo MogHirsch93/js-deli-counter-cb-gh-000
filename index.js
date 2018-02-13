c4f2afef48e9059d6a7a8c0dfeaea254f673ad2c
Morgan V Hirsch <morganhirsch93@hotmail.co.uk>
2018-02-13 16:23:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nowServing(katzDeliLine){
   if(katzDeliLine.length > 1){
   let firstInQueue = katzDeliLine[0]
   katzDeliLine.shift();
-  return `Currently serving ${firstInQueue} `
+  return `Currently serving ${firstInQueue}.`
   } else{
   return "There is nobody waiting to be served!"
   }
