4a44dc1712e2fabc92cf1d0ae569d2c6ed3f5601
Morgan V Hirsch <morganhirsch93@hotmail.co.uk>
2018-02-13 16:09:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const katzDeli = [];
 
 function takeANumber(katzDeliLine, newCustomer){
     katzDeliLine.push(newCustomer);
-  return `You are number ${katzDeliLine[katzDeliLine.length-1]}`
+    return `You are number ${katzDeliLine[katzDeliLine.length-1]}`
 }
