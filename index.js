2cc1297fd42c1699d7bd6b166b071436117f0baf
Morgan V Hirsch <morganhirsch93@hotmail.co.uk>
2018-02-13 16:13:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const katzDeli = [];
 
 function takeANumber(katzDeliLine, newCustomer){
     katzDeliLine.push(newCustomer);
-    return `Welcome, ${newCustomer}. You are number ${katzDeliLine.length-1} in line.`
+    return `Welcome, ${newCustomer}. You are number ${katzDeliLine.length} in line.`
 }
